15cf6a8e5b06b907d98acea587b33905ae985dbd
a0323022@gmail.com <a0323022@gmail.com>
2020-03-29 12:13:17 -0500

    032920

---
+++ justright.py
@@ -0,0 +1,4 @@
+money = 400
+if (money >= 100 and money <= 500) or (money >= 1000 and money <= 5000):
+    print('Money is between 100 and 500 and between 1000 and 5000')
+
---
+++ twinkies.py
@@ -0,0 +1,5 @@
+twinkies = 1000
+if twinkies < 100:
+    print("Too few!")
+else:
+    print("Too many!")
