f384c81c909b53c8f9ff2bb737d2a1d7e71bb770
Trey <a0323022@gmail.com>
2020-07-09 21:52:55 -0500

    Create evenodd.py

---
+++ evenodd.py
@@ -0,0 +1,21 @@
+age = int(input('Enter your age:'))
+if age % == 0
+  print('odd')
+  
+  Ensure that you have performed the following tasks before you upgrade:
+
+Obtain a backup of the ISE configuration and operational data.
+
+Obtain a backup of the system logs.
+
+Disable scheduled backups. Reconfigure the backup schedules after deployment upgrade is complete.
+
+Export the certificates and private keys.
+
+Configure a repository. Download the upgrade bundle and place it in the repository.
+
+Make a note of Active Directory join credentials and RSA SecurID node secret, if applicable. You need this information to connect to Active Directory or RSA SecurID server after upgrade.
+
+Purge the operational data to improve upgrade performance.
+
+Ensure that your Internet connection to the repository is good.
