8f01032738c1c6769ceb25966a2151b8231d06e6
Trey <a0323022@gmail.com>
2020-07-24 15:33:32 -0500

    Update and rename PracExer1 to PracExer1.py

---
+++ PracExer1.py
@@ -0,0 +1,18 @@
+
+  
+animal = 'Dog'
+veggie = 'asparagus'
+mineral = 'gypsum'
+
+print('Here is an animal, veggie, and a mineral.')
+print(animal)
+print(veggie)
+print(mineral)
+
+
+#if ninjas < 10:
+ #   print("I can fight those ninjas!")
+#elif ninjas < 30:
+ #   print("It'll be a struggle, but I can take 'em!")
+#elif ninjas < 50:
+ #   print("That's too many!")
