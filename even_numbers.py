787690047012812d688dccbb287ea929b5da55ca
Trey <a0323022@gmail.com>
2022-04-14 12:48:25 -0500

    041422

---
+++ even_numbers.py
@@ -0,0 +1,16 @@
+	#1: Even Numbers
+	#Create a loop that prints even numbers until it reaches your year of age or,
+    # if your age is an odd number, prints out odd numbers until it reaches your age. 
+
+num = 0
+age = int((input("Enter your age:")))
+while num <= age:
+    if (age % 2) == 0
+        print('They are even!')
+        print(num + 2)
+    else
+        print('They are odd!')
+        print(num + 1)
+    num+=
+
+
